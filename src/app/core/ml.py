7d4bf68ec9be59278e61ae2a69504ab3b88054ee
EgorTarasov <61450170+EgorTarasov@users.noreply.github.com>
2023-03-25 20:43:43 +0300

    new metrics (migth not work) :)

--- src/app/core/ml.py
+++ src/app/core/ml.py
@@ -77,11 +77,149 @@ def shops_manufacturer(dict1: dict, dict2: dict) -> dict:
     info = dict()
     for i in shop_id["region_code"].unique():
         data = shop_id[shop_id["region_code"] == i][:5][["id_sp_", "dt"]]
-        id = data["id_sp_"].values
-        dt = data["dt"].values
+        id = list(map(str, data["id_sp_"].values))
+        dt = list(map(int, data["dt"].values))
         info[i] = {
-            "id": id,  # id магазина
+            "id": id,  # id магазина (object)
             "count": dt,
-        }  # кол-во выведенных из оборота товаров
+        }  # кол-во выведенных из оборота товаров (int)
 
     return info
+
+
+def volumes_manufacturer_region(dict1: dict, dict2: dict) -> dict:
+    """Количество единиц товара и стоимость всего проданного товара для 1 производителя по регионам"""
+
+    dop_data = pd.DataFrame(dict1)
+    shops = pd.DataFrame(dict2)
+    dop_data.dropna(inplace=True)
+    shops = shops[["id_sp_", "region_code", "city_with_type"]]
+    dop_data_merged = pd.merge(dop_data, shops, on="id_sp_", how="left")
+    dop_data_merged["dt"] = pd.to_datetime(dop_data_merged["dt"])
+    dop_data_merged.sort_values(by=["dt"], inplace=True)
+    dop_data_merged = dop_data_merged[dop_data_merged["dt"] >= "2022-01-01"]
+    dop_data_merged["sum_price"] = dop_data_merged["price"] * dop_data_merged["cnt"]
+    dop_data_new = dop_data_merged.drop(columns=["prid", "inn", "id_sp_", "price"])
+    dop_data_new.drop(columns=["city_with_type"], inplace=True)
+    dop_data_region = dop_data_new.groupby(["dt", "region_code"]).sum().reset_index()
+    dop_data_region["Месяц"] = dop_data_region["dt"].dt.month
+    data_month_region = (
+        dop_data_region.groupby(["region_code", "Месяц"])
+        .sum()[["cnt", "sum_price"]]
+        .reset_index()
+    )
+
+    info = dict()
+    for i in data_month_region["region_code"].unique():
+        data = data_month_region[data_month_region["region_code"] == i]
+        month = list(data["Месяц"].astype("int").values)
+        count = list(data["cnt"].astype("int").values)
+        sum_price = list(data["sum_price"].astype("int").values)
+        info[i] = {
+            "month": month,  # месяц
+            "count": count,  # кол-во выведенного из оборота товара
+            "sum_price": sum_price,
+        }  # суммарная цена товаров
+
+    return info
+
+
+def volumes_manufacturer(dict1: dict, dict2: dict) -> dict:
+    """Количество единиц товара и стоимость всего проданного товара для 1 производителя в целом"""
+
+    dop_data = pd.DataFrame(dict1)
+    shops = pd.DataFrame(dict2)
+    dop_data.dropna(inplace=True)
+    shops = shops[["id_sp_", "region_code", "city_with_type"]]
+    dop_data_merged = pd.merge(dop_data, shops, on="id_sp_", how="left")
+    dop_data_merged["dt"] = pd.to_datetime(dop_data_merged["dt"])
+    dop_data_merged.sort_values(by=["dt"], inplace=True)
+    dop_data_merged = dop_data_merged[dop_data_merged["dt"] >= "2022-01-01"]
+    dop_data_merged["sum_price"] = dop_data_merged["price"] * dop_data_merged["cnt"]
+    dop_data_new = dop_data_merged.drop(columns=["prid", "inn", "id_sp_", "price"])
+    dop_data_new.drop(columns=["city_with_type"], inplace=True)
+    dop_data_region = dop_data_new.groupby(["dt", "region_code"]).sum().reset_index()
+    dop_data_region["Месяц"] = dop_data_region["dt"].dt.month
+    data_month = (
+        dop_data_region.groupby(["Месяц"]).sum()[["cnt", "sum_price"]].reset_index()
+    )
+
+    data = data_month.copy()
+    month = list(data["Месяц"].astype("int").values)
+    count = list(data["cnt"].astype("int").values)
+    sum_price = list(data["sum_price"].astype("int").values)
+    info = {
+        "month": month,  # месяц
+        "count": count,  # кол-во выведенного из оборота товара
+        "sum_price": sum_price,
+    }  # суммарная цена товаров
+
+    return info
+
+
+def popular_offline_gtin_manufacturer_region(dict1: dict, dict2: dict) -> dict:
+    """Самые популярные товары среди оффлайн покупателей
+    для 1 производителя по регионам"""
+
+    dop_data = pd.DataFrame(dict1)
+    shops = pd.DataFrame(dict2)
+    dop_data.dropna(inplace=True)
+    shops = shops[["id_sp_", "region_code"]]
+    dop_data_merged = pd.merge(dop_data, shops, on="id_sp_", how="left")
+    dop_data_merged["dt"] = pd.to_datetime(dop_data_merged["dt"])
+    dop_data_merged.sort_values(by=["dt"], inplace=True)
+    dop_data_merged = dop_data_merged[dop_data_merged["dt"] >= "2022-01-01"]
+    dop_data_merged["sum_price"] = dop_data_merged["price"] * dop_data_merged["cnt"]
+    tab = dop_data_merged[
+        dop_data_merged["type_operation"]
+        == "Продажа конечному потребителю в точке продаж"
+    ]
+    tab.drop(columns=["price"], inplace=True)
+    groups = tab.groupby(["region_code", "gtin"]).sum().reset_index()
+    popular = groups.sort_values(by=["cnt"], ascending=False)[
+        ["region_code", "gtin", "cnt"]
+    ]
+
+    info_popular = dict()
+    for i in popular["region_code"].unique():
+        data = popular[popular["region_code"] == i][:5][["gtin", "cnt"]]
+        gtin = list(data["gtin"].astype("str").values)
+        cnt = list(data["cnt"].astype("int").values)
+        info_popular[i] = {
+            "gtin": gtin,  # gtin товара
+            "count": cnt,
+        }  # кол-во товара проданного оффлайн
+
+    return info_popular
+
+
+def popular_offline_gtin_manufacturer(dict1: dict, dict2: dict) -> dict:
+    """Самые популярные товары среди оффлайн покупателей
+    для 1 производителя в целом"""
+
+    dop_data = pd.DataFrame(dict1)
+    shops = pd.DataFrame(dict2)
+    dop_data.dropna(inplace=True)
+    shops = shops[["id_sp_", "region_code"]]
+    dop_data_merged = pd.merge(dop_data, shops, on="id_sp_", how="left")
+    dop_data_merged["dt"] = pd.to_datetime(dop_data_merged["dt"])
+    dop_data_merged.sort_values(by=["dt"], inplace=True)
+    dop_data_merged = dop_data_merged[dop_data_merged["dt"] >= "2022-01-01"]
+    dop_data_merged["sum_price"] = dop_data_merged["price"] * dop_data_merged["cnt"]
+    tab = dop_data_merged[
+        dop_data_merged["type_operation"]
+        == "Продажа конечному потребителю в точке продаж"
+    ]
+    tab.drop(columns=["price"], inplace=True)
+    groups = tab.groupby(["gtin"]).sum().reset_index()
+    popular = groups.sort_values(by=["cnt"], ascending=False)[["gtin", "cnt"]]
+
+    data = popular[:5][["gtin", "cnt"]]
+    gtin = list(data["gtin"].astype("str").values)
+    cnt = list(data["cnt"].astype("int").values)
+    info_popular = {
+        "gtin": gtin,  # gtin товара
+        "count": cnt,
+    }  # кол-во товара проданного оффлайн
+
+    return info_popular
--- src/app/endpoints/ml.py
+++ src/app/endpoints/ml.py
@@ -4,7 +4,14 @@ from sqlalchemy.orm import Session
 from app.core.dependencies import get_db
 from app.core.auth import oauth2_scheme, get_current_user
 from app.core.crud import get_points
-from app.core.ml import Model, shops_manufacturer
+from app.core.ml import (
+    Model,
+    shops_manufacturer,
+    volumes_manufacturer_region,
+    volumes_manufacturer,
+    popular_offline_gtin_manufacturer_region,
+    popular_offline_gtin_manufacturer,
+)
 
 from app.utils.logging import log
 
@@ -85,7 +92,227 @@ async def get_mertics(token=Depends(oauth2_scheme), db: Session = Depends(get_db
     log.info(len(result))
     print(result)
     log.info(f"calculated in {perf_counter() - start}")
-    return {"status": "ok"}
+    return result
+
+
+@router.get("/volumes_manufacturer_region")
+async def get_volume_metrics_by_region(
+    token=Depends(oauth2_scheme), db: Session = Depends(get_db)
+):
+    # rewrite sql query
+    user = get_current_user(db, token)
+    start = perf_counter()
+    log.info("computing...")
+
+    sold_data = {
+        "dt": [],
+        "gtin": [],
+        "prid": [],
+        "inn": [],
+        "id_sp_": [],
+        "type_operation": [],
+        "price": [],
+        "cnt": [],
+    }
+    for i in user.sold_goods:
+        sold_data["dt"].append(i.dt)
+        sold_data["gtin"].append(i.gtin)
+        sold_data["prid"].append(i.prid)
+        sold_data["inn"].append(i.inn)
+        sold_data["id_sp_"].append(i.id_sp_)
+        sold_data["type_operation"].append(i.type_operation)
+        sold_data["price"].append(i.price)
+        sold_data["cnt"].append(i.cnt)
+
+    additional_data = {
+        "id_sp_": [],
+        "inn": [],
+        "region_code": [],
+        "city_with_type": [],
+        "city_fias_id": [],
+        "postal_code": [],
+    }
+    points = get_points(db)
+    for i in points:
+        additional_data["id_sp_"].append(i.id_sp_)
+        additional_data["inn"].append(i.inn)
+        additional_data["region_code"].append(i.region_code)
+        additional_data["city_with_type"].append(i.city_with_type)
+        additional_data["city_fias_id"].append(i.city_fias_id)
+        additional_data["postal_code"].append(i.postal_code)
+    #
+    log.info("prepared data")
+
+    result = volumes_manufacturer_region(sold_data, additional_data)
+    log.info(len(result))
+    print(result)
+    log.info(f"calculated in {perf_counter() - start}")
+    return result
+
+
+@router.get("/volumes_manufacturer")
+async def get_volume_metrics(
+    token=Depends(oauth2_scheme), db: Session = Depends(get_db)
+):
+    # rewrite sql query
+    user = get_current_user(db, token)
+    start = perf_counter()
+    log.info("computing...")
+
+    sold_data = {
+        "dt": [],
+        "gtin": [],
+        "prid": [],
+        "inn": [],
+        "id_sp_": [],
+        "type_operation": [],
+        "price": [],
+        "cnt": [],
+    }
+    for i in user.sold_goods:
+        sold_data["dt"].append(i.dt)
+        sold_data["gtin"].append(i.gtin)
+        sold_data["prid"].append(i.prid)
+        sold_data["inn"].append(i.inn)
+        sold_data["id_sp_"].append(i.id_sp_)
+        sold_data["type_operation"].append(i.type_operation)
+        sold_data["price"].append(i.price)
+        sold_data["cnt"].append(i.cnt)
+
+    additional_data = {
+        "id_sp_": [],
+        "inn": [],
+        "region_code": [],
+        "city_with_type": [],
+        "city_fias_id": [],
+        "postal_code": [],
+    }
+    points = get_points(db)
+    for i in points:
+        additional_data["id_sp_"].append(i.id_sp_)
+        additional_data["inn"].append(i.inn)
+        additional_data["region_code"].append(i.region_code)
+        additional_data["city_with_type"].append(i.city_with_type)
+        additional_data["city_fias_id"].append(i.city_fias_id)
+        additional_data["postal_code"].append(i.postal_code)
+    #
+    log.info("prepared data")
+
+    result = volumes_manufacturer(sold_data, additional_data)
+    log.info(len(result))
+    print(result)
+    log.info(f"calculated in {perf_counter() - start}")
+    return result
+
+
+@router.get("/popular_offline_gtin_manufacturer_region")
+async def get_popular_offline_metrics_by_region(
+    token=Depends(oauth2_scheme), db: Session = Depends(get_db)
+):
+    # rewrite sql query
+    user = get_current_user(db, token)
+    start = perf_counter()
+    log.info("computing...")
+
+    sold_data = {
+        "dt": [],
+        "gtin": [],
+        "prid": [],
+        "inn": [],
+        "id_sp_": [],
+        "type_operation": [],
+        "price": [],
+        "cnt": [],
+    }
+    for i in user.sold_goods:
+        sold_data["dt"].append(i.dt)
+        sold_data["gtin"].append(i.gtin)
+        sold_data["prid"].append(i.prid)
+        sold_data["inn"].append(i.inn)
+        sold_data["id_sp_"].append(i.id_sp_)
+        sold_data["type_operation"].append(i.type_operation)
+        sold_data["price"].append(i.price)
+        sold_data["cnt"].append(i.cnt)
+
+    additional_data = {
+        "id_sp_": [],
+        "inn": [],
+        "region_code": [],
+        "city_with_type": [],
+        "city_fias_id": [],
+        "postal_code": [],
+    }
+    points = get_points(db)
+    for i in points:
+        additional_data["id_sp_"].append(i.id_sp_)
+        additional_data["inn"].append(i.inn)
+        additional_data["region_code"].append(i.region_code)
+        additional_data["city_with_type"].append(i.city_with_type)
+        additional_data["city_fias_id"].append(i.city_fias_id)
+        additional_data["postal_code"].append(i.postal_code)
+    #
+    log.info("prepared data")
+
+    result = popular_offline_gtin_manufacturer_region(sold_data, additional_data)
+    log.info(len(result))
+    print(result)
+    log.info(f"calculated in {perf_counter() - start}")
+    return result
+
+
+@router.get("/popular_offline_gtin_manufacturer")
+async def get_popular_offline_metrics(
+    token=Depends(oauth2_scheme), db: Session = Depends(get_db)
+):
+    # rewrite sql query
+    user = get_current_user(db, token)
+    start = perf_counter()
+    log.info("computing...")
+
+    sold_data = {
+        "dt": [],
+        "gtin": [],
+        "prid": [],
+        "inn": [],
+        "id_sp_": [],
+        "type_operation": [],
+        "price": [],
+        "cnt": [],
+    }
+    for i in user.sold_goods:
+        sold_data["dt"].append(i.dt)
+        sold_data["gtin"].append(i.gtin)
+        sold_data["prid"].append(i.prid)
+        sold_data["inn"].append(i.inn)
+        sold_data["id_sp_"].append(i.id_sp_)
+        sold_data["type_operation"].append(i.type_operation)
+        sold_data["price"].append(i.price)
+        sold_data["cnt"].append(i.cnt)
+
+    additional_data = {
+        "id_sp_": [],
+        "inn": [],
+        "region_code": [],
+        "city_with_type": [],
+        "city_fias_id": [],
+        "postal_code": [],
+    }
+    points = get_points(db)
+    for i in points:
+        additional_data["id_sp_"].append(i.id_sp_)
+        additional_data["inn"].append(i.inn)
+        additional_data["region_code"].append(i.region_code)
+        additional_data["city_with_type"].append(i.city_with_type)
+        additional_data["city_fias_id"].append(i.city_fias_id)
+        additional_data["postal_code"].append(i.postal_code)
+    #
+    log.info("prepared data")
+
+    result = popular_offline_gtin_manufacturer(sold_data, additional_data)
+    log.info(len(result))
+    print(result)
+    log.info(f"calculated in {perf_counter() - start}")
+    return result
 
 
 # @router.post("/create_user", response_model=UserSchema)
